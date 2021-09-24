c8be7f7a2197f02757d164ee02d590a00e026e03
Stanley Filipe <stanley92filipe@gmail.com>
2021-09-24 12:01:37 -0300

    add namespaces in the websocket server, update index.js file

--- index.js
+++ index.js
@@ -1,21 +1,24 @@
-const express = require('express');
-const app = express();
-const http = require('http');
-const server = http.createServer(app);
-const { Server } = require("socket.io");
-const io = new Server(server);
-const PORT = process.env.PORT || 3000;
+const express = require('express')
+const app = express()
+const http = require('http')
+const server = http.createServer(app)
+const { Server } = require("socket.io")
+const io = new Server(server)
+const PORT = process.env.PORT || 3000
 
 app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/index.html');
+  res.sendFile(__dirname + '/index.html')
+})
+
+io.of(/^\/[a-z]{3}-[a-z]{4}-[a-z]{3}$/)
+  .on('connection', function(socket) {
+
+    socket.on('new-message', function(data) {
+      socket.broadcast.emit('new-message', data)
+    });
+    
 });
 
-io.on('connection', (socket) => {
-  console.log('a user connected');
-  socket.on("new-message", (msg) => {
-      io.emit("new-message", msg)
-  })
-});
 
 server.listen(PORT, () => {
   console.log(`listening on http://localhost:3000`);
