63a6eab1277ea0a7e2ee2854bfa485c232289a5e
Stanley Filipe <stanley92filipe@gmail.com>
2021-09-25 22:30:17 -0300

    update a socket.io server, add a shipping confirmation

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
 const http = require('http')
 const server = http.createServer(app)
-const { Server } = require("socket.io")
+const { Server } = require('socket.io')
 const io = new Server(server)
 const PORT = process.env.PORT || 3000
 
@@ -10,16 +10,21 @@ app.get('/', (req, res) => {
   res.sendFile(__dirname + '/index.html')
 })
 
-io.of(/^\/[a-z]{3}-[a-z]{4}-[a-z]{3}$/)
-  .on('connection', function(socket) {
+const namespace = io.of(/^\/[a-z]{3}-[a-z]{4}-[a-z]{3}$/)
 
-    socket.on('new-message', function(data) {
-      socket.broadcast.emit('new-message', data)
-    });
+namespace.on('connection', (socket) => {
+
+    socket.on('new-message', (msg) => {
+      socket.broadcast.emit('new-message', msg)
+      socket.emit('uploaded-message', msg.id)
+    })
     
-});
+    socket.on('message-received', (id_msg) => {
+      socket.broadcast.emit('message-received', id_msg)
+    })
 
+})
 
 server.listen(PORT, () => {
-  console.log(`listening on http://localhost:3000`);
-});
+  console.log(`listening on http://localhost:${PORT}`)
+})
