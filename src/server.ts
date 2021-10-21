29d9f1802706e0e8e5300f11ccb5137d1a5ef647
Stanley Filipe <stanley92filipe@gmail.com>
2021-10-21 00:44:34 -0300

    add eslint

--- src/server.ts
+++ src/server.ts
@@ -7,7 +7,7 @@ const server = http.createServer(app)
 const io = new Server(server)
 
 const PORT = process.env.PORT || 3000
- 
+
 app.get('/', (request, response) => {
   response.send('Não há nada para ver aqui')
 })
@@ -15,15 +15,13 @@ app.get('/', (request, response) => {
 const namespace = io.of('chat')
 
 namespace.on('connection', (socket) => {
-
-	socket.on('join-room', room => {
-		socket.join(room)
-	})
+  socket.on('join-room', room => {
+    socket.join(room)
+  })
 
   socket.on('new-message', (msg) => {
     socket.to(msg.to).emit('new-message', msg)
   })
-
 })
 
 server.listen(PORT, () => {
